f08158b7e4986f63cd5f395b7136392b0bf6a211
luccamourao <lfaria@epfl.ch>
2023-12-04 13:14:05 +0100

    added ICMonActor class

---
+++ icmon/src/main/java/ch/epfl/cs107/icmon/actor/ICMonActor.java
@@ -0,0 +1,99 @@
+package ch.epfl.cs107.icmon.actor;
+
+import ch.epfl.cs107.play.areagame.actor.Interactable;
+import ch.epfl.cs107.play.areagame.actor.MovableAreaEntity;
+import ch.epfl.cs107.play.areagame.area.Area;
+import ch.epfl.cs107.play.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.engine.actor.Sprite;
+import ch.epfl.cs107.play.engine.actor.TextGraphics;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Orientation;
+import ch.epfl.cs107.play.math.Vector;
+import ch.epfl.cs107.play.window.Canvas;
+
+import java.awt.*;
+import java.util.Collections;
+import java.util.List;
+
+abstract public class ICMonActor extends MovableAreaEntity{
+    public ICMonActor(Area owner, Orientation orientation, DiscreteCoordinates coordinates) {
+        super(owner, orientation, coordinates);
+
+
+
+    }
+
+    /**
+     * ???
+     //* @param canvas target, not null
+     */
+    public void draw() {
+
+    }
+
+    /**
+     * ???
+     * @return ???
+     */
+    @Override
+    public boolean takeCellSpace() {
+        return false;
+    }
+
+    /**
+     * ???
+     * @return ???
+     */
+    @Override
+    public boolean isCellInteractable() {
+        return true;
+    }
+
+    /**
+     * ???
+     * @return ???
+     */
+    @Override
+    public boolean isViewInteractable() {
+        return true;
+    }
+
+    /**
+     * ???
+     */
+    @Override
+    public List<DiscreteCoordinates> getCurrentCells() {
+        return Collections.singletonList(getCurrentMainCellCoordinates());
+    }
+
+    /**
+     * ???
+     * @param v (AreaInteractionVisitor) : the visitor
+     * @param isCellInteraction ???
+     */
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction) {
+
+    }
+
+    /**
+     * Leave an area by unregister this player
+     */
+    public void leaveArea() {
+        getOwnerArea().unregisterActor(this);
+    }
+
+    /**
+     * ???
+     * @param area     (Area): initial area, not null
+     * @param position (DiscreteCoordinates): initial position, not null
+     */
+    public void enterArea(Area area, DiscreteCoordinates position) {
+        area.registerActor(this);
+        area.setViewCandidate(this);
+        setOwnerArea(area);
+        setCurrentPosition(position.toVector());
+        resetMotion();
+    }
+
+}
