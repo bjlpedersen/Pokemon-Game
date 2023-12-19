1e1eb4a8de61609f0f984416e07f5843e4110479
bjork <bjlpedersen@gmail.com>
2023-12-19 14:51:28 +0100

    4.3.2 Door and interactions working again

--- icmon/src/main/java/ch/epfl/cs107/icmon/actor/ICMonPlayer.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/actor/ICMonPlayer.java
@@ -44,51 +44,57 @@ import java.util.List;
  */
 public final class ICMonPlayer extends ICMonActor implements Interactor, Interactable {
 
-    /** ??? */
+    /**
+     * ???
+     */
     private final static int MOVE_DURATION = 8;
     /** ??? */
 
-    /** ??? */
+    /**
+     * ???
+     */
     private boolean isInDialog = false;
     private final Sprite sprite;
-    /** ??? */
+    /**
+     * ???
+     */
     private float hp;
     private ICMonPlayerInteractionHandler handler;
     private OrientedAnimation currentAnimation;
     private final OrientedAnimation landAnimation;
     private final OrientedAnimation waterAnimation;
-    
+
     private ICMon.ICMonGameState gameState;
     private ICMon icMon;
     private Dialog dialog;
     private List<Pokemon> collection = new ArrayList<Pokemon>();
 
-    
-
 
     /**
      * ???
-     * @param owner ???
+     *
+     * @param owner       ???
      * @param orientation ???
      * @param coordinates ???
-     * @param spriteName ???
+     * @param spriteName  ???
      */
     public ICMonPlayer(ICMon icMon, Area owner, Orientation orientation, DiscreteCoordinates coordinates, String spriteName, Pokemon starterPokemon) {
         super(owner, orientation, coordinates);
         sprite = new Sprite(spriteName, 1.f, 1.f, this);
         collection.add(starterPokemon);
         orientation = getOrientation();
-        landAnimation = new OrientedAnimation("actors/player",MOVE_DURATION, orientation, this);
-        waterAnimation = new OrientedAnimation("actors/player_water",MOVE_DURATION, orientation, this);
+        landAnimation = new OrientedAnimation("actors/player", MOVE_DURATION, orientation, this);
+        waterAnimation = new OrientedAnimation("actors/player_water", MOVE_DURATION, orientation, this);
         currentAnimation = landAnimation;
 
         this.icMon = icMon;
-        
+
         this.gameState = icMon.getGameState(); // get the game state from the ICMon instance
         handler = new ICMonPlayerInteractionHandler();
 
 
     }
+
     public class ICMonPlayerInteractionHandler implements ICMonInteractionVisitor {
 
         public void interactWith(ICMonBehavior.ICMonCell cell, boolean isCellInteraction) {
@@ -96,20 +102,19 @@ public final class ICMonPlayer extends ICMonActor implements Interactor, Interac
                 ICMonBehavior.AllowedWalkingType walkingType = cell.getAllowedWalkingType();
 //                System.out.println("Detected Cell Type: " + walkingType); // Debug output
 
-                if(walkingType==ICMonBehavior.AllowedWalkingType.FEET) {
+                if (walkingType == ICMonBehavior.AllowedWalkingType.FEET) {
                     currentAnimation = landAnimation;
                     currentAnimation.orientate(getOrientation());
 
-                } else if(walkingType==ICMonBehavior.AllowedWalkingType.SURF){
-                        currentAnimation = waterAnimation;
-                        currentAnimation.orientate(getOrientation());
-                    }
+                } else if (walkingType == ICMonBehavior.AllowedWalkingType.SURF) {
+                    currentAnimation = waterAnimation;
+                    currentAnimation.orientate(getOrientation());
+                }
             }
         }
 
 
-
-        public void interactWith(ICBall ball, boolean wantsViewInteraction){
+        public void interactWith(ICBall ball, boolean wantsViewInteraction) {
             if (wantsViewInteraction) {
                 CollectItemEvent event = new CollectItemEvent(ball, icMon.getEventManager(), ICMonPlayer.this);
                 ball.collect();
@@ -123,196 +128,195 @@ public final class ICMonPlayer extends ICMonActor implements Interactor, Interac
             if (wantsCellInteraction()) {
                 GamePlayMessage message = new SuspendWithEvent(event);
                 gameState.send(message);
-                }
             }
         }
 
-        public void interactWith(ICShopAssistant assistant , boolean wantsViewInteraction) {
+
+        public void interactWith(ICShopAssistant assistant, boolean wantsViewInteraction) {
             if (wantsViewInteraction()) {
                 isInDialog = true;
                 dialog = new Dialog("end_of_game_event_interaction_with_icshopassistant");
                 //start();
 //
                 //new LogAction("This is an interaction between the player and ICShopAssistant based on events").perform();
-                
+
+            }
+
         }
-        
+
+        public void interactWith(Door door, boolean isCellInteraction) {
+            if (isCellInteraction) {
+                GamePlayMessage message = new PassDoorMessage(door);
+                gameState.send(message);
+            }
+        }
     }
-    public void interactWith(Door door, boolean isCellInteraction) {
-        if (isCellInteraction) {
-            GamePlayMessage message = new PassDoorMessage(door);
-            gameState.send(message);
+
+
+        private void fight(ICMonFightableActor opponent) {
+        }
+
+
+        public void draw(Canvas canvas) {
+            currentAnimation.draw(canvas);
+            if (isInDialog) {
+                dialog.draw(canvas);
             }
         }
 
+        /**
+         * ???
+         *
+         * @param deltaTime elapsed time since last update, in seconds, non-negative
+         */
 
-private void fight(ICMonFightableActor opponent) {}
+        public void update(float deltaTime) {
+            Keyboard keyboard = getOwnerArea().getKeyboard();
 
+            if (isInDialog) {
+                if (keyboard.get(Keyboard.SPACE).isPressed()) {
+                    dialog.update(deltaTime);
+                }
+                if (dialog.isCompleted()) {
+                    isInDialog = false;
+                }
+            } else {
+                moveIfPressed(Orientation.LEFT, keyboard.get(Keyboard.LEFT));
+                moveIfPressed(Orientation.UP, keyboard.get(Keyboard.UP));
+                moveIfPressed(Orientation.RIGHT, keyboard.get(Keyboard.RIGHT));
+                moveIfPressed(Orientation.DOWN, keyboard.get(Keyboard.DOWN));
+                super.update(deltaTime);
 
-    @Override
-public void draw(Canvas canvas){
-    currentAnimation.draw(canvas);
-    if(isInDialog){
-        dialog.draw(canvas);
-    }
-}
+                if (keyboard.get(Keyboard.LEFT).isDown() || keyboard.get(Keyboard.UP).isDown()
+                        || keyboard.get(Keyboard.RIGHT).isDown() || keyboard.get(Keyboard.DOWN).isDown()) {
 
-    /**
-     * ???
-     * @param deltaTime elapsed time since last update, in seconds, non-negative
-     */
+                    currentAnimation.update(deltaTime);
+                    currentAnimation.orientate(getOrientation());
 
-     @Override
-     public void update(float deltaTime) {
-         Keyboard keyboard = getOwnerArea().getKeyboard();
+                } else {
+                    currentAnimation.reset();
+                }
+            }
+        }
 
-         if(isInDialog){
-             if(keyboard.get(Keyboard.SPACE).isPressed()){
-                 dialog.update(deltaTime);
-             }
-             if(dialog.isCompleted()){
-                 isInDialog = false;
-             }
-         } else {
-             moveIfPressed(Orientation.LEFT, keyboard.get(Keyboard.LEFT));
-             moveIfPressed(Orientation.UP, keyboard.get(Keyboard.UP));
-             moveIfPressed(Orientation.RIGHT, keyboard.get(Keyboard.RIGHT));
-             moveIfPressed(Orientation.DOWN, keyboard.get(Keyboard.DOWN));
-             super.update(deltaTime);
+        public void draw() {
+        }
 
-             if(keyboard.get(Keyboard.LEFT).isDown() || keyboard.get(Keyboard.UP).isDown()
-                     || keyboard.get(Keyboard.RIGHT).isDown() ||keyboard.get(Keyboard.DOWN).isDown()){
+        /**
+         * ???
+         *
+         * @return ???
+         */
+        public boolean takeCellSpace() {
+            return true;
+        }
 
-                 currentAnimation.update(deltaTime);
-                 currentAnimation.orientate(getOrientation());
+        /**
+         * ???
+         *
+         * @return ???
+         */
+        public boolean isCellInteractable() {
+            return true;
+        }
 
-             } else {
-                 currentAnimation.reset();
-             }
-         }
-     }
+        /**
+         * ???
+         *
+         * @return ???
+         */
+        public boolean isViewInteractable() {
+            return false;
+        }
 
-    @Override
-    public void draw() {
-    }
+        /**
+         * ???
+         */
+        public List<DiscreteCoordinates> getCurrentCells() {
+            return Collections.singletonList(getCurrentMainCellCoordinates());
+        }
 
-    /**
-     * ???
-     * @return ???
-     */
-    @Override
-    public boolean takeCellSpace() {
-        return true;
-    }
+        public List<DiscreteCoordinates> getFieldOfViewCells() {
+            return Collections.singletonList(getCurrentMainCellCoordinates().jump(getOrientation().toVector()));
+        }
 
-    /**
-     * ???
-     * @return ???
-     */
-    @Override
-    public boolean isCellInteractable() {
-        return true;
-    }
+        public boolean wantsCellInteraction() {
+            return true;
+        }
 
-    /**
-     * ???
-     * @return ???
-     */
-    @Override
-    public boolean isViewInteractable() {
-        return false;
-    }
-
-    /**
-     * ???
-     */
-    @Override
-    public List<DiscreteCoordinates> getCurrentCells() {
-        return Collections.singletonList(getCurrentMainCellCoordinates());
-    }
-
-    @Override
-    public List<DiscreteCoordinates> getFieldOfViewCells() {
-        return Collections.singletonList(getCurrentMainCellCoordinates().jump(getOrientation().toVector()));
-    }
-
-    @Override
-    public boolean wantsCellInteraction() {
-        return true;
-    }
-
-    @Override
-public boolean wantsViewInteraction() {
-    Keyboard keyboard = getOwnerArea().getKeyboard();
-    return !isInDialog && keyboard.get(Keyboard.L).isDown();
-}
+        public boolean wantsViewInteraction() {
+            Keyboard keyboard = getOwnerArea().getKeyboard();
+            return !isInDialog && keyboard.get(Keyboard.L).isDown();
+        }
 
 
-     @Override
+        @Override
         public void interactWith(Interactable other, boolean isCellInteraction) {
             other.acceptInteraction(handler, isCellInteraction);
 
         }
 
 
-    /**
-     * ???
-     * @param v (AreaInteractionVisitor) : the visitor
-     * @param isCellInteraction ???
-     */
+        /**
+         * ???
+         *
+         * @param v                 (AreaInteractionVisitor) : the visitor
+         * @param isCellInteraction ???
+         */
 
-    public void acceptInteraction(ICMonInteractionVisitor v, boolean isCellInteraction) {
-        ((ICMonInteractionVisitor) v).interactWith(this, isCellInteraction);
-    }
+        public void acceptInteraction(ICMonInteractionVisitor v, boolean isCellInteraction) {
+            ((ICMonInteractionVisitor) v).interactWith(this, isCellInteraction);
+        }
 
-    /**
-     * Orientate and Move this player in the given orientation if the given button is down
-     *
-     * @param orientation (Orientation): given orientation, not null
-     * @param b           (Button): button corresponding to the given orientation, not null
-     */
-    private void moveIfPressed(Orientation orientation, Button b) {
-        if (b.isDown()) {
-            if (!isDisplacementOccurs()) {
-                orientate(orientation);
-                move(MOVE_DURATION);
+        /**
+         * Orientate and Move this player in the given orientation if the given button is down
+         *
+         * @param orientation (Orientation): given orientation, not null
+         * @param b           (Button): button corresponding to the given orientation, not null
+         */
+        private void moveIfPressed(Orientation orientation, Button b) {
+            if (b.isDown()) {
+                if (!isDisplacementOccurs()) {
+                    orientate(orientation);
+                    move(MOVE_DURATION);
 
+                }
             }
+
+        }
+
+        /**
+         * Leave an area by unregister this player
+         */
+        public void leaveArea() {
+            getOwnerArea().unregisterActor(this);
+        }
+
+        /**
+         * ???
+         *
+         * @param area     (Area): initial area, not null
+         * @param position (DiscreteCoordinates): initial position, not null
+         */
+        public void enterArea(Area area, DiscreteCoordinates position) {
+            area.registerActor(this);
+            area.setViewCandidate(this);
+
+            setOwnerArea(area);
+            setCurrentPosition(position.toVector());
+            resetMotion();
+        }
+
+        /**
+         * ???
+         * @return ???
+         */
+
+        /**
+         * Center the camera on the player
+         */
+        public void centerCamera() {
+            getOwnerArea().setViewCandidate(this);
         }
 
     }
-
-    /**
-     * Leave an area by unregister this player
-     */
-    public void leaveArea() {
-        getOwnerArea().unregisterActor(this);
-    }
-
-    /**
-     * ???
-     * @param area     (Area): initial area, not null
-     * @param position (DiscreteCoordinates): initial position, not null
-     */
-    public void enterArea(Area area, DiscreteCoordinates position) {
-        area.registerActor(this);
-        area.setViewCandidate(this);
-        
-        setOwnerArea(area);
-        setCurrentPosition(position.toVector());
-        resetMotion();
-    }
-
-    /**
-     * ???
-     * @return ???
-     */
-
-    /**
-     * Center the camera on the player
-     */
-    public void centerCamera() {
-        getOwnerArea().setViewCandidate(this);
-    }
-
-}
